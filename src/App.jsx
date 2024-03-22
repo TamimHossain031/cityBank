7d0a3c836cf7636093272f9bd695a46e392d51d9
Tamim <tamimhossain031@gmail.com>
2024-03-22 23:38:04 +0600

    add all

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hero from "./components/Hero";
+import Menu from "./components/Menu";
+import Notice from "./components/NoticeBox";
+
+function App() {
+  return (
+    <>
+      <section className="">
+        <Hero/>
+        <Menu/>
+        <Notice/>
+      </section>
+    </>
+  );
+}
+
+export default App;
